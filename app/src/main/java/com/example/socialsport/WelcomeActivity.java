3c86b62e920585febe94bb81c4e27225258ed17d
VinceDollo <vincent.dollo@hotmail.fr>
2022-03-09 00:36:22 -0500

    Debut

---
+++ app/src/main/java/com/example/socialsport/WelcomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.socialsport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btn_go_login_in, btn_go_register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_activity);
+
+        btn_go_login_in = findViewById(R.id.btn_go_log_in);
+        btn_go_register = findViewById(R.id.btn_go_register);
+
+        btn_go_login_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent IntentLoginActivity = new Intent(getApplicationContext(), loginActivity.class);
+                startActivity(IntentLoginActivity);
+            }
+        });
+        //TODO
+        btn_go_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                Toast.makeText(getApplicationContext(),"TODO", Toast.LENGTH_SHORT);
+            }
+        }
+        );
+    }
+
+
+}
---
+++ app/src/main/java/com/example/socialsport/loginActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialsport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_activity);
+    }
+}
