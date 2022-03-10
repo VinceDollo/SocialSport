716ea103137380c6e8e1cee75349f968fe044e1d
VinceDollo <vincent.dollo@hotmail.fr>
2022-03-10 00:47:21 -0500

    test map

--- app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
+++ app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
@@ -3,6 +3,7 @@ package com.example.socialsport;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class PrincipalPageActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class PrincipalPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_page_activity);
+
+        Fragment fragment = new MapsFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_map,fragment).commit();
     }
 }
