edea848704cab163328fcc6fba4b5458da078465
mezianekhodja <meziane.khodja@etudiant.univ-rennes1.fr>
2022-03-10 12:33:58 -0500

    et search localisation

--- app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
+++ app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
@@ -1,11 +1,15 @@
 package com.example.socialsport;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class PrincipalPageActivity extends AppCompatActivity {
+    EditText et_localisation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,20 @@ public class PrincipalPageActivity extends AppCompatActivity {
 
         Fragment fragment = new MapsFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.fl_map,fragment).commit();
+        et_localisation = (EditText) findViewById(R.id.et_search_city);
+
+        et_localisation.setOnKeyListener((v, keyCode, event) -> {
+            // If the event is a key-down event on the "enter" button
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                //Save the answer
+                String localisation=et_localisation.getText().toString();
+                // Perform action on key press
+                Toast.makeText(this, localisation, Toast.LENGTH_SHORT).show();
+                //Delete text from edit text
+                et_localisation.setText("");
+                return true;
+            }
+            return false;
+        });
     }
 }
