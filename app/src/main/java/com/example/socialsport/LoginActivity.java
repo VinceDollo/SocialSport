328b496f3dc1a01d7c28f15e04e8de8a55c05d26
ImArxus <alex.lebalanger@icloud.com>
2022-03-09 19:41:59 -0500

    Suppression de la plupart des warnings

--- app/src/main/java/com/example/socialsport/LoginActivity.java
+++ app/src/main/java/com/example/socialsport/LoginActivity.java
@@ -1,9 +1,5 @@
 package com.example.socialsport;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,9 +10,8 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -25,9 +20,8 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private EditText et_email, et_password;
     private Button btn_log_in;
-    private ImageButton btn_back;
 
-    private View.OnClickListener onClickListener = new View.OnClickListener() {
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             //TODO with better way
@@ -35,23 +29,19 @@ public class LoginActivity extends AppCompatActivity {
             String password = et_password.getText().toString();
 
             mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("LoginPage", "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Toast.makeText(LoginActivity.this, "Authentication succesful.",
-                                        Toast.LENGTH_SHORT).show();
-                                //updateUI(user);
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("LoginPage", "signInWithEmail:failure", task.getException());
-                                Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                // updateUI(null);
-                            }
+                    .addOnCompleteListener(LoginActivity.this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("LoginPage", "signInWithEmail:success");
+                            Toast.makeText(LoginActivity.this, "Authentication succesful.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("LoginPage", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            // updateUI(null);
                         }
                     });
         }
@@ -65,31 +55,26 @@ public class LoginActivity extends AppCompatActivity {
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
         btn_log_in = findViewById(R.id.btn_log_in);
-        btn_back = findViewById(R.id.btn_back);
+        ImageButton btn_back = findViewById(R.id.btn_back);
         btn_log_in.setOnClickListener(onClickListener);
 
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent IntentWelcomeActivity = new Intent(getApplicationContext(), WelcomeActivity.class);
-                startActivity(IntentWelcomeActivity);
-                finish();
-            }
+        btn_back.setOnClickListener(view -> {
+            Intent IntentWelcomeActivity = new Intent(getApplicationContext(), WelcomeActivity.class);
+            startActivity(IntentWelcomeActivity);
+            finish();
         });
 
         //When you push enter button
-        et_password.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                // If the event is a key-down event on the "enter" button
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    // Perform action on key press
-                    btn_log_in.performClick();
-                    return true;
-                }
-                return false;
+        et_password.setOnKeyListener((v, keyCode, event) -> {
+            // If the event is a key-down event on the "enter" button
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Perform action on key press
+                btn_log_in.performClick();
+                return true;
             }
+            return false;
         });
     }
 
@@ -100,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            // the user is already signed in
+            System.out.println(currentUser);
         }
     }
 }
--- app/src/main/java/com/example/socialsport/RegisterActivity.java
+++ app/src/main/java/com/example/socialsport/RegisterActivity.java
@@ -5,35 +5,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.Toast;
 
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.socialsport.entities.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.stream.IntStream;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private EditText et_email, et_password, et_checkpassword, et_name, et_age;
     private Button btn_log_in;
-    private ImageButton btn_back;
 
     private void writeUserToDatabase(FirebaseDatabase database, String email, String name, String age, String uid) {
         DatabaseReference myRef = database.getReference();
@@ -43,20 +32,17 @@ public class RegisterActivity extends AppCompatActivity {
 
     private void getUserFromDatabase(FirebaseDatabase database, String uid) {
         DatabaseReference myRef = database.getReference();
-        myRef.child("users").child(uid).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    Log.e("firebase", "Error getting data", task.getException());
-                } else {
-                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
-                }
+        myRef.child("users").child(uid).get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e("firebase", "Error getting data", task.getException());
+            } else {
+                Log.d("firebase", String.valueOf(task.getResult().getValue()));
             }
         });
 
     }
 
-    private View.OnClickListener onClickListener = new View.OnClickListener() {
+    private final View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             //TODO with better way
@@ -67,27 +53,25 @@ public class RegisterActivity extends AppCompatActivity {
             String age = et_age.getText().toString();
 
             mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // Sign in success, update UI with the signed-in user's information
-                                Log.d("Sign up page", "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Toast.makeText(RegisterActivity.this, "Authentication success.",
-                                        Toast.LENGTH_SHORT).show();
-                                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                                writeUserToDatabase(database, user.getEmail(), name, age, user.getUid());
-                                getUserFromDatabase(database, user.getUid());
+                    .addOnCompleteListener(RegisterActivity.this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Sign up page", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseDatabase database = FirebaseDatabase.getInstance();
+                            assert user != null;
+                            writeUserToDatabase(database, user.getEmail(), name, age, user.getUid());
+                            getUserFromDatabase(database, user.getUid());
 
-                                //TODO on complete sign up treatment
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w("Sign up page", "createUserWithEmail:failure", task.getException());
-                                Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                                //TODO on failed sign up treatment
-                            }
+                            //TODO on complete sign up treatment
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Sign up page", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //TODO on failed sign up treatment
                         }
                     });
         }
@@ -105,30 +89,25 @@ public class RegisterActivity extends AppCompatActivity {
         et_age = findViewById(R.id.et_age);
 
         btn_log_in = findViewById(R.id.btn_log_in);
-        btn_back = findViewById(R.id.btn_back);
+        ImageButton btn_back = findViewById(R.id.btn_back);
         btn_log_in.setOnClickListener(onClickListener);
 
 
-        btn_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent IntentWelcomeActivity = new Intent(getApplicationContext(), WelcomeActivity.class);
-                startActivity(IntentWelcomeActivity);
-                finish();
-            }
+        btn_back.setOnClickListener(view -> {
+            Intent IntentWelcomeActivity = new Intent(getApplicationContext(), WelcomeActivity.class);
+            startActivity(IntentWelcomeActivity);
+            finish();
         });
 
-        et_password.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                // If the event is a key-down event on the "enter" button
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    // Perform action on key press
-                    btn_log_in.performClick();
-                    return true;
-                }
-                return false;
+        et_password.setOnKeyListener((v, keyCode, event) -> {
+            // If the event is a key-down event on the "enter" button
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Perform action on key press
+                btn_log_in.performClick();
+                return true;
             }
+            return false;
         });
     }
 
@@ -139,7 +118,7 @@ public class RegisterActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            // the user is already signed in
+            System.out.println(currentUser);
         }
     }
 }
