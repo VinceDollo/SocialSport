de44c4e8c2cd98943b9cb469e9402fb692e37038
VinceDollo <vincent.dollo@hotmail.fr>
2022-03-14 18:12:12 -0400

    ajout de fragment

---
+++ app/src/main/java/com/example/socialsport/HomeFragment.java
@@ -0,0 +1,87 @@
+package com.example.socialsport;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        SupportMapFragment mMapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.f_maps);
+        mMapFragment.getMapAsync(this);
+
+
+        EditText et_localisation = (EditText) view.findViewById(R.id.et_search_city);
+        et_localisation.setOnKeyListener((v, keyCode, event) -> {
+            // If the event is a key-down event on the "enter" button
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                //Save the answer
+                String localisation = et_localisation.getText().toString();
+                //Delete text from edit text
+                Geocoder gc = new Geocoder(getContext());
+                double locationName;
+                try {
+                    List<Address> addresses = gc.getFromLocationName(localisation, 1);
+                    double lattitude = addresses.get(0).getLatitude();
+                    double longitude = addresses.get(0).getLongitude();
+                    Toast.makeText(getActivity(), Double.toString(lattitude) + " " + Double.toString(longitude), Toast.LENGTH_SHORT).show();
+                    LatLng placeToFind = new LatLng(lattitude, longitude);
+                    //googleMap.addMarker(new MarkerOptions().position(placeToFind).title(localisation));
+                    //googleMap.moveCamera(CameraUpdateFactory.newLatLng(placeToFind));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Error place doesn't exist", Toast.LENGTH_SHORT).show();
+                }
+                et_localisation.setText("");
+
+                return true;
+            }
+            return false;
+        });
+        return view;
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+    }
+}
--- app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
+++ app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
@@ -4,8 +4,9 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.Window;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,72 +14,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
-import com.example.socialsport.databinding.PrincipalPageActivityBinding;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
-import java.io.IOException;
-import java.util.List;
+public class PrincipalPageActivity extends FragmentActivity {
 
-public class PrincipalPageActivity extends AppCompatActivity implements OnMapReadyCallback {
-    private EditText et_localisation;
-    private GoogleMap mMap;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //  requestWindowFeature(Window.FEATURE_NO_TITLE);
-      //  this.getSupportActionBar().hide();
-
         setContentView(R.layout.principal_page_activity);
+        bottomNavigationView = findViewById(R.id.bottom_app_bar);
+        getSupportFragmentManager().beginTransaction().replace(R.id.principal_page,new HomeFragment()).commit();
 
 
-        // Get the SupportMapFragment and request notification when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.f_maps);
-        mapFragment.getMapAsync(this);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment =  null;
+                switch (item.getItemId()){
+                    case R.id.appBar_personn:
+                        fragment = new PersonFragment();
+                        break;
+                    case R.id.appBar_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.appBar_message:
+                        fragment = new MessageFragment();
+                        break;
 
-        et_localisation = (EditText) findViewById(R.id.et_search_city);
-
-        et_localisation.setOnKeyListener((v, keyCode, event) -> {
-            // If the event is a key-down event on the "enter" button
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                //Save the answer
-                String localisation=et_localisation.getText().toString();
-                //Delete text from edit text
-                et_localisation.setText("");
-                Geocoder gc = new Geocoder(PrincipalPageActivity.this);
-                double locationName;
-                try {
-                    List<Address> addresses = gc.getFromLocationName(localisation, 1);
-                    double lattitude = addresses.get(0).getLatitude();
-                    double longitude = addresses.get(0).getLongitude();
-                    Toast.makeText(this, Double.toString(lattitude)+" "+Double.toString(longitude), Toast.LENGTH_SHORT).show();
-                    LatLng placeToFind = new LatLng(lattitude, longitude);
-                    //googleMap.addMarker(new MarkerOptions().position(placeToFind).title(localisation));
-                    //googleMap.moveCamera(CameraUpdateFactory.newLatLng(placeToFind));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, "Error place doesn't exist", Toast.LENGTH_SHORT).show();
                 }
+                getSupportFragmentManager().beginTransaction().replace(R.id.principal_page, fragment).commit();
                 return true;
             }
-            return false;
         });
     }
 
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 }
