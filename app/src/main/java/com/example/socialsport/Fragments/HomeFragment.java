17860a80b8319bf3e17d36ac4ae537632587b87f
ImArxus <alex.lebalanger@icloud.com>
2022-03-16 14:38:53 -0400

    Réglages bugs carte sur page principale

--- app/src/main/java/com/example/socialsport/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/socialsport/Fragments/HomeFragment.java
@@ -1,19 +1,22 @@
 package com.example.socialsport.Fragments;
 
+import android.annotation.SuppressLint;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.example.socialsport.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -27,54 +30,47 @@ import java.io.IOException;
 import java.util.List;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback {
-    private GoogleMap mMap;
 
+    private GoogleMap mMap;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
         // Get the SupportMapFragment and request notification when the map is ready to be used.
         SupportMapFragment mMapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.f_maps);
+        assert mMapFragment != null;
         mMapFragment.getMapAsync(this);
 
-
-
-        EditText et_localisation = (EditText) view.findViewById(R.id.et_search_city);
-        et_localisation.setOnKeyListener((v, keyCode, event) -> {
-            // If the event is a key-down event on the "enter" button
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                //Save the answer
-                String localisation = et_localisation.getText().toString();
-                Geocoder gc = new Geocoder(getContext());
-                try {
-                    List<Address> addresses = gc.getFromLocationName(localisation, 1);
-                    double lattitude = addresses.get(0).getLatitude();
-                    double longitude = addresses.get(0).getLongitude();
-                    Toast.makeText(getActivity(), lattitude + " " + longitude, Toast.LENGTH_SHORT).show();
-                    LatLng placeToFind = new LatLng(lattitude, longitude);
-                    mMap.addMarker(new MarkerOptions().position(placeToFind).title("What you searched"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(placeToFind));
-
-                } catch (IOException e) {
-                    Log.d("TAG", e.toString());
-                    e.printStackTrace();
-                    Toast.makeText(getActivity(), "Error place doesn't exist", Toast.LENGTH_SHORT).show();
-                }
-                //Delete text from edit text
-                et_localisation.setText("");
-
-                return true;
+        // Allow vertical scroll in map fragment
+        ScrollView scroll = (ScrollView) view.findViewById(R.id.scrollView);
+        ImageView transparent = (ImageView) view.findViewById(R.id.imagetrans);
+        transparent.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            switch (action) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_MOVE:
+                    // Disallow ScrollView to intercept touch events.
+                    scroll.requestDisallowInterceptTouchEvent(true);
+                    // Disable touch on transparent view
+                    return false;
+                case MotionEvent.ACTION_UP:
+                    // Allow ScrollView to intercept touch events.
+                    scroll.requestDisallowInterceptTouchEvent(false);
+                    return true;
+                default:
+                    return true;
             }
-            return false;
         });
-        return view;
 
+        return view;
     }
 
     @Override
@@ -86,5 +82,42 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+        searchPlaceListener();
     }
+
+    public void searchPlaceListener() {
+        // Create searching localisation method
+        EditText et_localisation = (EditText) view.findViewById(R.id.et_search_city);
+        et_localisation.setOnKeyListener((v, keyCode, event) -> {
+            // If the event is a key-down event on the "enter" button
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                //Save the answer
+                String localisation = et_localisation.getText().toString();
+                Geocoder gc = new Geocoder(getContext());
+                try {
+                    List<Address> addresses = gc.getFromLocationName(localisation, 1);
+                    if (addresses.isEmpty())
+                        Toast.makeText(getActivity(), "Error: the searched place doesn't exist", Toast.LENGTH_SHORT).show();
+                    else {
+                        double latitude = addresses.get(0).getLatitude();
+                        double longitude = addresses.get(0).getLongitude();
+                        LatLng placeToFind = new LatLng(latitude, longitude);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(placeToFind, 12));
+                    }
+                } catch (IOException e) {
+                    Log.d("Place_error", e.toString());
+                    e.printStackTrace();
+                }
+
+                //Delete text from edit text
+                et_localisation.setText("");
+
+                return true;
+            }
+            return false;
+        });
+    }
+
+
+
 }
--- app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
+++ app/src/main/java/com/example/socialsport/PrincipalPageActivity.java
@@ -1,7 +1,6 @@
 package com.example.socialsport;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -11,20 +10,15 @@ import com.example.socialsport.Fragments.HomeFragment;
 import com.example.socialsport.Fragments.MessageFragment;
 import com.example.socialsport.Fragments.PersonFragment;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
-
 
 public class PrincipalPageActivity extends FragmentActivity {
-
-    private MeowBottomNavigation meowBottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal_page_activity);
 
-        meowBottomNavigation = findViewById(R.id.bottom_app_bar);
+        MeowBottomNavigation meowBottomNavigation = findViewById(R.id.bottom_app_bar);
 
         meowBottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.img_home));
         meowBottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.img_message));
@@ -32,30 +26,26 @@ public class PrincipalPageActivity extends FragmentActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
 
-        meowBottomNavigation.show(1,true);
-        meowBottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
-            @Override
-            public Unit invoke(MeowBottomNavigation.Model model) {
-                switch (model.getId()) {
-                    case 1:
-                        replace(new HomeFragment());
-                        break;
-                    case 2:
-                        replace(new MessageFragment());
-                        break;
-                    case 3:
-                        replace(new PersonFragment());
-                        break;
-                }
-                return null;
+        meowBottomNavigation.show(1, true);
+        meowBottomNavigation.setOnClickMenuListener(model -> {
+            switch (model.getId()) {
+                case 1:
+                    replace(new HomeFragment());
+                    break;
+                case 2:
+                    replace(new MessageFragment());
+                    break;
+                case 3:
+                    replace(new PersonFragment());
+                    break;
             }
+            return null;
         });
 
     }
 
-    private void replace(Fragment fragment){
+    private void replace(Fragment fragment) {
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, fragment).commit();
     }
-
 
 }
