0382cd89d2bb94d9f54780994680c3aad291ac05
EloyMartinez <eloyalbmartinez@gmail.com>
2022-03-20 20:34:33 -0400

    added activity firebase management

--- app/src/main/java/com/example/socialsport/Fragments/PlaceActivityFragment.java
+++ app/src/main/java/com/example/socialsport/Fragments/PlaceActivityFragment.java
@@ -1,6 +1,7 @@
 package com.example.socialsport.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +20,10 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.socialsport.LoginActivity;
 import com.example.socialsport.R;
+import com.example.socialsport.entities.SportActivity;
+import com.example.socialsport.entities.User;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -27,6 +32,10 @@ import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PlaceActivityFragment extends Fragment implements OnMapReadyCallback {
 
@@ -34,9 +43,17 @@ public class PlaceActivityFragment extends Fragment implements OnMapReadyCallbac
     private ImageButton btn_back;
     private Button btn_validate;
     private TextView tv_title;
+    private FirebaseAuth mAuth;
+
 
     private LatLng current_latLng;
     private String sport;
+
+    private void writeActivityToDatabase(FirebaseDatabase database, String description, String date, String heure, String coords, String currentUserID) {
+        DatabaseReference myRef = database.getReference();
+        SportActivity newActivity = new SportActivity(description, date, heure, currentUserID,coords);
+        myRef.child("activities").push().setValue(newActivity);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,7 +70,7 @@ public class PlaceActivityFragment extends Fragment implements OnMapReadyCallbac
 
         //recupere le sport
         Bundle bundle = this.getArguments();
-        if(bundle != null){
+        if (bundle != null) {
             sport = bundle.getString("sport");
             tv_title.setText("Choose location for " + sport);
         }
@@ -75,9 +92,23 @@ public class PlaceActivityFragment extends Fragment implements OnMapReadyCallbac
             public void onClick(View view) {
                 //add information to next fragment
                 Bundle bundle = new Bundle();
-                bundle.putString("sport",sport);
+                bundle.putString("sport", sport);
                 bundle.putString("location", String.valueOf(current_latLng));
                 Fragment newF = new DescriptionActivityFragment();
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                if (currentUser == null) {
+                    Intent i = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(i);
+                    Toast.makeText(getActivity(), "Sorry, you need to log back in to your account",
+                            Toast.LENGTH_SHORT).show();
+                }
+                Log.d("coords debug poing",String.valueOf(current_latLng));
+                Log.d("debug uid client",currentUser.getUid());
+
+               writeActivityToDatabase(database,"","","",String.valueOf(current_latLng), currentUser.getUid());
+
                 newF.setArguments(bundle);
                 getParentFragmentManager().beginTransaction().replace(R.id.frameLayout, newF).commit();
             }
@@ -96,7 +127,7 @@ public class PlaceActivityFragment extends Fragment implements OnMapReadyCallbac
         MarkerOptions marker = new MarkerOptions();
 
         BitmapDescriptor icon = checkIcon();
-        if(icon != null){
+        if (icon != null) {
             //Toast.makeText(getActivity(), icon.toString(), Toast.LENGTH_LONG).show();
             mMap.addMarker(marker.position(current_latLng).title("default").icon(icon));
         } else {
@@ -120,10 +151,10 @@ public class PlaceActivityFragment extends Fragment implements OnMapReadyCallbac
 
     }
 
-    private BitmapDescriptor checkIcon(){
+    private BitmapDescriptor checkIcon() {
         BitmapDescriptor icon = null;
         Toast.makeText(getActivity(), sport, Toast.LENGTH_LONG).show();
-        switch (sport){
+        switch (sport) {
             case "football":
                 icon = BitmapDescriptorFactory.fromResource(R.drawable.img_football);
                 break;
