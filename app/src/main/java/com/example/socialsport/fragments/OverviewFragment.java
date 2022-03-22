1eded8f4b179e8ab8b4e47da710775f500313d7c
ImArxus <alex.lebalanger@icloud.com>
2022-03-22 16:35:54 -0400

    Affichage participants mis à jour

--- app/src/main/java/com/example/socialsport/fragments/OverviewFragment.java
+++ app/src/main/java/com/example/socialsport/fragments/OverviewFragment.java
@@ -18,16 +18,13 @@ import androidx.fragment.app.Fragment;
 
 import com.example.socialsport.R;
 import com.example.socialsport.Utils;
-import com.example.socialsport.entities.User;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
@@ -48,27 +45,28 @@ public class OverviewFragment extends Fragment {
     TextView dateTime;
     TextView location;
     CircleImageView imgOrganiser;
+    ArrayList<String> participantsUuids;
 
-    private void queryDbUsers(Bundle bundle) {
+
+    private void queryDbUsers() {
         DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
         DatabaseReference activitiesRef = rootRef.child("activities");
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                ArrayList<String> participantsUuids = bundle.getStringArrayList("participants");
+                tableLayout.removeAllViews();
                 Log.d(tag, participantsUuids.toString());
                 for (String participantUuid : participantsUuids) {
                     //TODO: error while displaying name of a new participant
-                    //TODO: find a better way to avoid attach a listener for each iteration
                     FirebaseDatabase.getInstance().getReference().child("users").child(participantUuid).get().addOnCompleteListener(task -> {
                         if (!task.isSuccessful()) {
                             Log.e(tag, "Error getting data", task.getException());
                         } else {
                             @SuppressLint("InflateParams") TableRow row = (TableRow) LayoutInflater.from(getActivity()).inflate(R.layout.table_row, null);
-                            Log.d(tag, String.valueOf(task.getResult().getValue()));
                             HashMap<String, String> participant = (HashMap<String, String>) Objects.requireNonNull(task.getResult().getValue());
                             //TODO: add person image
                             Log.d(tag, participant.toString());
+                            Log.d(tag, "");
                             ((TextView) row.findViewById(R.id.name_participant)).setText(participant.get("name"));
                             tableLayout.addView(row);
                         }
@@ -88,19 +86,17 @@ public class OverviewFragment extends Fragment {
             if (!task.isSuccessful()) {
                 Log.e(tag, "Error getting data", task.getException());
             } else {
-                Log.d(tag, "" + task.getResult().getValue().getClass());
+                Log.d(tag, ">" + Objects.requireNonNull(task.getResult().getValue()).getClass());
                 ArrayList<String> currentParticipants = (ArrayList<String>) task.getResult().getValue();
-                if (!currentParticipants.contains(mAuth.getCurrentUser().getUid())) {
+                if (!currentParticipants.contains(Objects.requireNonNull(mAuth.getCurrentUser()).getUid())) {
                     currentParticipants.add(mAuth.getCurrentUser().getUid());
-                    FirebaseDatabase.getInstance().getReference().child("activities").child(activityID).child("uuids").setValue(currentParticipants);
                 } else {
                     currentParticipants.remove(mAuth.getCurrentUser().getUid());
-                    FirebaseDatabase.getInstance().getReference().child("activities").child(activityID).child("uuids").setValue(currentParticipants);
                 }
+                participantsUuids = currentParticipants;
+                FirebaseDatabase.getInstance().getReference().child("activities").child(activityID).child("uuids").setValue(currentParticipants);
             }
         });
-
-
     }
 
     @Nullable
@@ -117,22 +113,14 @@ public class OverviewFragment extends Fragment {
         location = view.findViewById(R.id.tv_location);
         imgOrganiser = view.findViewById(R.id.img_organiser);
 
-
         btnBack.setOnClickListener(view1 -> getParentFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit());
-
 
         Bundle bundle = this.getArguments();
         if (bundle != null) {
-            queryDbUsers(bundle);
+            participantsUuids = bundle.getStringArrayList("participants");
+            queryDbUsers();
 
-            btnParticipate.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    handleParticipate(bundle.getString("activityID"));
-                }
-            });
-            ;
-
+            btnParticipate.setOnClickListener(view -> handleParticipate(bundle.getString("activityID")));
 
             String sport = bundle.getString("sport");
             imgSport.setImageBitmap(Utils.getBitmap(getActivity(), sport));
