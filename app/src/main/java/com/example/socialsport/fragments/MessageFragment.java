a6622522075b23b5b3a50aa4a0b2ac24666849b3
VinceDollo <vincent.dollo@hotmail.fr>
2022-04-19 15:51:15 -0400

    fini avec les binding

--- app/src/main/java/com/example/socialsport/fragments/MessageFragment.java
+++ app/src/main/java/com/example/socialsport/fragments/MessageFragment.java
@@ -4,10 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.socialsport.databinding.FragmentMessageBinding;
 import com.example.socialsport.utils.ListAdapter;
 import com.example.socialsport.R;
 import com.example.socialsport.activities.PrincipalPageActivity;
@@ -19,20 +20,20 @@ public class MessageFragment extends Fragment {
     private final int[] images = {R.drawable.img_football,R.drawable.img_football,R.drawable.img_football,R.drawable.img_football,R.drawable.img_football};
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_message, container, false);
+        com.example.socialsport.databinding.FragmentMessageBinding binding = FragmentMessageBinding.inflate(inflater);
+        View view = binding.getRoot();
 
         ((PrincipalPageActivity) requireActivity()).getMeowBottomNavigation().show(2,true);
 
-        ListView lvChat = view.findViewById(R.id.lv_chat);
 
         ListAdapter lAdapter = new ListAdapter(getContext(), ((PrincipalPageActivity) requireActivity()).getMessagesMap(), images);
 
-        lvChat.setAdapter(lAdapter);
+        binding.lvChat.setAdapter(lAdapter);
 
-        lvChat.setOnItemClickListener((adapterView, view1, i, l) -> {
+        binding.lvChat.setOnItemClickListener((adapterView, view1, i, l) -> {
             Object firstKey = ((PrincipalPageActivity) requireActivity()).getMessagesMap().keySet().toArray()[i];
             Bundle bundle = new Bundle();
             bundle.putString("name", (String) firstKey);
